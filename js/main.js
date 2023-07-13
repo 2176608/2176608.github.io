c3125155cdf0ebb1d99c22ad7d1ef7e367479a20
2176608 <ycj2176608@outlook.com>
2023-07-13 20:42:35 +0800

    Site updated: 2023-07-13 20:42:34

--- js/main.js
+++ js/main.js
@@ -825,38 +825,3 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
-var callback = function (args) {
-  console.log(args)
-  console.log('验证成功');
-};
-var expiredCallback = function (args) {
-  console.log(args)
-  console.log('验证过期');
-};
-var errorCallback = function (args) {
-  console.log(args)
-  console.log('验证失败');
-};
-
-var widgetId;
-var onloadCallback = function () {
-  // 得到组件id
-  widgetId = grecaptcha.render('robot', {
-      'sitekey': '6LeeGREnAAAAAOFGmU8XGzyXZbYfJc60U6_Olqmx', 
-      'theme': 'light',
-      'size': 'compact',
-      'callback': callback, 
-      'expired-callback': expiredCallback, 
-      'error-callback': errorCallback 
-  });
-};
-
-function getResponseFromRecaptcha() {
-  var responseToken = grecaptcha.getResponse(widgetId);
-  if (responseToken.length == 0) {
-      alert("验证失败");
-  } else {
-      alert("验证通过");
-      
-  }
-};
